ff9517e13765d8c2d10fe599e9d9626d5af8e1ca
ismailovwd <ismailovwd@gmail.com>
2025-11-01 17:15:26 +0500

    push

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
 
-  // Global API prefix
+  app.use('/', express.static(join(__dirname, '..', 'public')));
+  app.use('/public', express.static(join(__dirname, '..', 'public')));
   app.setGlobalPrefix('api');
 
-  server.use(express.static(join(__dirname, '..', 'dist')));
-
-  server.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '..', 'dist', 'index.html'));
-  });
-
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
